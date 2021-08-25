d123ddec2089e8744a247a6fce257a9e74af870f
Daniel Leppert <dan.uu.econ@gmail.com>
2021-08-25 19:49:56 +0100

    added touch screen functionality

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ board.forEach(row => {
     row.forEach(tile => {
       bonusText.textContent = yieldCount(board)
       boardElement.append(tile.element)
-      tile.element.addEventListener("click", () => {
+      tile.element.addEventListener("touchstart", () => {
         markTile(board, tile)
       })
       tile.element.addEventListener("contextmenu", e => {
